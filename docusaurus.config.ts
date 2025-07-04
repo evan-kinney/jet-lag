20463b604549cfba4fac3597b8242c76380465c8
Evan Kinney <evandashkinney@icloud.com>
2025-07-04 10:21:40 -0400

    Fix GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://evan-kinney.github.io',
+	url: process.env.URL || 'https://evan-kinney.github.io',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: process.env.BASE_URL || '/',
